4df04e1e6132c9243fc890bdf1706dc0c91275f7
msingh025 <memaneesh@hotmail.com>
2021-06-06 05:25:26 +0530

    update, insert with ORM

--- build/index.js
+++ build/index.js
@@ -10,9 +10,11 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
+const config_1 = require("./core/config");
 const sequelize_1 = require("./sequelize");
+const user_1 = require("./models/user");
 const app = express();
-const port = 4001;
+const port = config_1.SERVER_PORT;
 sequelize_1.sequlize.authenticate().then((data) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         console.log("connection");
@@ -24,6 +26,24 @@ sequelize_1.sequlize.authenticate().then((data) => __awaiter(void 0, void 0, voi
 }));
 app.get('/', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     res.json('Hello world');
+}));
+app.post('/user', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(req.params);
+    const result = yield user_1.default.create({
+        first_name: "maneesh",
+        last_name: "singh",
+        email: "memaneesh@hotmail.com",
+        password: "Hello@1234"
+    });
+    console.log(result);
+    res.json({ status: 200 });
+}));
+app.put('/user', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield user_1.default.update({ first_name: "demo " }, { where: { id: 1 } });
+    res.json({
+        status: 200,
+        result: result
+    });
 }));
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import  { Request, Response, NextFunction } from 'express';
 import * as express from 'express';
-
-import {sequlize} from './sequelize'
+import {SERVER_PORT} from './core/config'
+import {sequlize} from './sequelize';
+import  User from './models/user';
 const app = express()
-const port = 4001
+const port = SERVER_PORT
 sequlize.authenticate().then(async (data)=>{
     try{ 
         console.log("connection");
@@ -16,7 +17,25 @@ app.get('/', async (req: any, res: any, next : any) => {
     
     res.json('Hello world')
 })
-
+app.post('/user', async (req: Request, res: Response, next: NextFunction) => {
+   console.log(req.params);
+   
+   const result = await User.create({
+    first_name: "maneesh",
+    last_name: "singh",
+    email: "memaneesh@hotmail.com",
+    password: "Hello@1234"
+   });
+   console.log(result);
+   res.json({status:200})
+});
+app.put('/user', async (req: Request, res: Response, next:NextFunction) =>{
+    const result= await User.update({first_name:"demo "}, {where:{id : 1}});
+    res.json({
+       status:200, 
+       result: result
+   });
+})
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`)
 })
